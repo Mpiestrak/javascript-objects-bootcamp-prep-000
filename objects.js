410ae906dd8e855309fceb5e0aec74ae18662407
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-05 00:43:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {"Phil Ochs": "Here's to the State of Mississippi", 'My Bloody Valentine': "Sometimes"};
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign ({}, playlist, {Slowdive: ["Alison"]})
@@ -6,4 +7,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, Slowdive){
   delete playlist.Slowdive
   return playlist
+=======
+var playlist = {Creed: "Higher"};
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign ({}, playlist, {'Phil Ochs': ['Slowdive']})
+  
+>>>>>>> 2657cffc23d3d4e02240c817f9fc91306f02c07c
 }
